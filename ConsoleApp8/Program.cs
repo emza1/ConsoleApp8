6589a4ab82f4b0a3d3bed410d5353350cb3b9ef3
lab_services_student <st10067195@rcconnect.edu.za>
2023-06-05 08:29:31 +0000

    Declared variables to Cap on first letters

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+    class Ingredient
+{
+    public string Name { get; set; }
+    public decimal Quantity { get; set; }
+    public string Unit { get; set; }
+    public Ingredient(string Name, decimal Quantity, string Unit)
+    {
+        Name = Name;
+        Quantity = Quantity;
+        Unit = Unit;
+    }
+    public override string ToString()
+    {
+        return $"{Quantity} {Unit} {Name}";
+    }
+}
+class Step
+{
+    public string Description { get; set; }
+    public Step(string description)
+    {
+        Description = description;
+    }
+    public override string ToString()
+    {
+        return Description;
+    }
+}
+class Recipe
+{
+    private Ingredient[] ingredients;
+    private Step[] steps;
+    public Recipe(int numIngredients, int numSteps)
+    {
+        ingredients = new Ingredient[numIngredients];
+        steps = new Step[numSteps];
+    }
+    public void AddIngredient(int index, string Name, decimal Quantity, string Unit)
+    {
+        ingredients[index] = new Ingredient(Name, Quantity, Unit);
+    }
+    public void AddStep(int index, string description)
+    {
+        steps[index] = new Step(description);
+    }
+    public void Display()
+    {
+        Console.WriteLine("Ingredients:");
+        foreach (Ingredient ingredient in ingredients)
+        {
+            Console.WriteLine(ingredient.ToString());
+        }
+        Console.WriteLine("Steps:");
+        for (int i = 0; i < steps.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {steps[i].ToString()}");
+        }
+    }
+    public void Scale(decimal factor)
+    {
+        foreach (Ingredient ingredient in ingredients)
+        {
+            ingredient.Quantity *= factor;
+        }
+    }
+    public void Reset()
+    {
+        foreach (Ingredient ingredient in ingredients)
+        {
+            ingredient.Quantity /= 3;
+            ingredient.Quantity /= 6;
+        }
+    }
+}
+
+
